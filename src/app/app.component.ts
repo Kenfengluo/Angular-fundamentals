a026cd1656fccb40d0664af38ae706c98b2c786b
AMERICA\LUOK1 <luok1@airproducts.com>
2018-09-27 14:53:25 -0400

    initial checkin and wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'events-app',
+  template:
+  `
+  <nav-bar></nav-bar>
+  <events-list></events-list>
+  `
 })
 export class AppComponent {
   title = 'angular-fundamentals';
